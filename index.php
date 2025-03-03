40c293dbc6dc201fdff040f6121b2217f141152c
doxifieq <doxifieq@gmail.com>
2025-03-03 11:31:36 +0100

    [update]

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
+<!DOCTYPE HTML>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-echo 'hello world';
+        <?php wp_head(); ?>
+    </head>
+
+    <body <?php body_class(); ?>>
+        <?php wp_body_open(); ?>
+
+        <h1>wordpress dev theme</h1>
+
+        <?php wp_footer(); ?>
+    </body>
+</html>

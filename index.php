ca2a1d7bd9e2cd6a6b7eeb27531fec421dede00e
doxifieq <doxifieq@gmail.com>
2025-03-03 13:20:22 +0100

    [update]

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+
+
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <?php wp_head(); ?>
+    <head>
+
+    <body <?php body_class(); ?>>
+        <?php wp_body_open(); ?>
+
+        <h1>wp dev</h1>
+
+        <?php wp_footer(); ?>
+    </body>
+<html>
